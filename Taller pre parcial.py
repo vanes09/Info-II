a104d1485f2c3a383aa47871c3ccecc32f29e71c
vanes09 <vanessa.hincapie1@udea.edu.co>
2025-05-14 14:33:05 -0500

    modificaciones para el punto 2

--- Taller pre parcial.py
+++ Taller pre parcial.py
@@ -2,5 +2,8 @@
 import numpy as np
 
 #matriz aleatioria de 4 dimendiones con size de 1200000
-np.random.rand(20,30,40,50)
+matriz = np.random.rand(20,30,40,50)
+
+copia_3dmatriz = matriz[0].copy()
+
 
