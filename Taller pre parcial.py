f0b184d1b95fa65b0257cd012d6d8f4713a6b4e5
vanes09 <vanessa.hincapie1@udea.edu.co>
2025-05-16 18:33:02 -0500

    Punto 4

--- Taller pre parcial.py
+++ Taller pre parcial.py
@@ -7,11 +7,15 @@ matriz = np.random.rand(20,30,40,50)
 #se crea la copia seleccionando la parte que deseo quitar
 copia_3d = matriz[0].copy()
 
+#Atributos de la matriz
 print("Atributos de la matriz")
 print("Forma (shape):", copia_3d.shape)        
 print("Número de dimensiones (ndim):", copia_3d.ndim) 
 print("Tamaño total (size):", copia_3d.size)    
-print("Tipo de datos (dtype):", copia_3d.dtype)  # float64 por defecto
+print("Tipo de datos (dtype):", copia_3d.dtype)  
 print("Tamaño de cada elemento (itemsize):", copia_3d.itemsize, "bytes")
 print("Memoria total ocupada (nbytes):", copia_3d.nbytes, "bytes")
 print("Tipo del objeto:", type(copia_3d))
+
+#Se modifica la matriz y se pasa a 2D
+copia_2D = copia_3d.reshape(1200,50)
